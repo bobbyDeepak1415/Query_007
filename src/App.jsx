6d3d220e6a25db35528d3d41e3ea69072e5b4916
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-07-16 02:29:06 +0530

    setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+
+const POSTS=[
+  {id:1,title:"post 1"},
+  {id:2,title:"post 2"},
+]
+
 function App() {
   return <h1>Hello</h1>;
+}
+
+
+function wait(duration){
+  return new Promise(resolve=>setTimeout(resolve,duration))
 }
 
 export default App;

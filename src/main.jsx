8c387215cdb57cf70c00edbe6cf5bc662e3e4175
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-08-13 15:29:20 +0530

    setup done

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import { QueryClientProvider,QueryClient } from "@tanstack/react-query";
 
+
+const querylient= new QueryClient()
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <QueryClientProvider>
+
       <App />
+    </QueryClientProvider>
   </StrictMode>,
 );

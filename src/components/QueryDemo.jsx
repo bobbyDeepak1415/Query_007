6463e4cf88ea3219523c6d7291cf78f1e56e0eeb
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-08-13 15:55:08 +0530

    setup done

---
+++ src/components/QueryDemo.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const QueryDemo = () => {
+
+    const url="https://jsonplaceholder.typicode.com/posts?_limit=5"
+
+    const [posts,setPosts]
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default QueryDemo

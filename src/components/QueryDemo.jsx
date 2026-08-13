c4e39b02661b27454924b49e3fa68e25472203f0
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-08-14 02:38:04 +0530

    setup done

--- src/components/QueryDemo.jsx
+++ src/components/QueryDemo.jsx
@@ -1,30 +1,28 @@
-import  { useState,useEffect } from 'react'
+import {useQuery} from '@tanstack/react-query'
 
 const url="https://jsonplaceholder.typicode.com/posts?_limit=5"
 
 
- const fetchPosts=async()=>{
+const fetchPosts=async()=>{
 
                 const response=await fetch(url)
-                if(!response.ok){
-                    throw new Error("Failed To fetch")
-                }
-                const res=await response.json()
-            
-            }
+               
+                return await response.json()
+          
+        }
 
 const QueryDemo = () => {
 
 
-   
-   
+    const {data:posts,isLoading,error}=useQuery({queryKey:["posts"],queryFn:fetchPosts})
+    
   return (
     <div>
         <h2>Intro and setUp</h2>
         {isLoading && <p>Loading</p>}
         {error && <p>Something went wrong...</p>}
         <ul>
-            {posts.map((post)=>{
+            {posts && posts.map((post)=>{
                 return <li key={post.id}>{post.title}</li>
             })}
         </ul>

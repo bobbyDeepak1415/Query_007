4f8a313f18d0e91f42ba6051b64b29c4b925cd32
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-08-13 15:58:52 +0530

    setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 
+// import {useQuery,useMutation} from '@tanstack/react-query'
+
+import QueryDemo from "./components/QueryDemo"
 
 function App() {
+
+ 
   return(
 
 <div>
 
   <h1>Hello</h1>
   <p>Hello There</p>
+  <QueryDemo/>
 </div>
   ) 
 }
 
 
 export default App
+ 
--- src/components/QueryDemo.jsx
+++ src/components/QueryDemo.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import  { useState,useEffect } from 'react'
 
 const QueryDemo = () => {
 
     const url="https://jsonplaceholder.typicode.com/posts?_limit=5"
 
-    const [posts,setPosts]
+    const [posts,setPosts]=useState([])
+
+    useEffect(()=>{
+
+        const fetchPosts=async()=>{
+            const response=await fetch(url)
+            const res=await response.json()
+            setPosts(res)
+        }
+        fetchPosts()
+
+    },[url])
 
   return (
     <div>
+        <h2>Intro and setUp</h2>
+        <ul>
+            {posts.map((post)=>{
+                return <li key={post.id}>{post.title}</li>
+            })}
+        </ul>
       
     </div>
   )

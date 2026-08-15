b9db948294ef9ee3d64680d8f63f57abf51de580
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-08-15 15:16:40 +0530

    setup done

--- src/components/QueryDemo.jsx
+++ src/components/QueryDemo.jsx
@@ -1,31 +1,45 @@
 
-import {useState} from 'react'
+import {useState,useEffect} from 'react'
 
-const url="https://jsonplaceholder.typicode.com/posts?_limit=5"
+const url="https://jsonplacehol...der.typicode.com/posts?_limit=5"
 
-import React from 'react'
 
 const QueryDemo = () => {
 
   const [posts,setPosts]=useState([])
+  const [isLoading,setIsLoading]=useState(true)
+  const [error,setError]=useState(null)
 
   useEffect(()=>{
-    const fetchData=async()=>{
-      const res=await fetch(url)
-const resposne=await res.json()
-setPosts(response)
+const fetchData=async()=>{
+      try{
+
+        const res=await fetch(url)
+        
+        if(!res.ok){
+          throw new Error("something went wrong")
+        }
+        const response=await res.json()
+        setPosts(response)
+      }catch(err){
+        setError(err)
+      }finally{
+        setIsLoading(false)
+      }
     }
 
     fetchData()
-  })
+  },[])
 
   return (
     <div>
-
+{isLoading && <p>Loading...</p>}
+{error && <p>Failed to fetch...</p>}
       <ul>
         {posts.map((post)=>{
           return <li key={post.id}>{post.title}</li>
         })}
+
       </ul>
       
     </div>

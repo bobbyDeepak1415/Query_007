b93ed01637728522c875bcba12b06871ac52302f
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-08-15 16:09:27 +0530

    setup done

--- src/components/QueryDemo.jsx
+++ src/components/QueryDemo.jsx
@@ -1,38 +1,25 @@
 
+import { useQuery } from '@tanstack/react-query'
 import {useState,useEffect} from 'react'
 
 const url="https://jsonplaceholder.typicode.com/posts?_limit=5"
 
 
-const QueryDemo = () => {
-
-  
-
-  const [posts,setPosts]=useState([])
-  const [isLoading,setIsLoading]=useState(true)
-  const [error,setError]=useState(null)
-
-  useEffect(()=>{
 const fetchData=async()=>{
-      try{
 
         const res=await fetch(url)
         
-        if(!res.ok){
-          throw new Error("something went wrong")
-        }
         const response=await res.json()
-        setPosts(response)
-      }catch(err){
-        setError(err)
-      }finally{
-        setIsLoading(false)
-      }
+        return response
     }
 
-    fetchData()
-  },[])
 
+const QueryDemo = () => {
+
+  
+const {data,error,isLoading}=useQuery({queryKey:["posts"],queryFn:fetchData})
+ 
+  
   return (
     <div>
 {isLoading && <p>Loading...</p>}

0242f280d37cf152cee4570e298546616a8db176
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-08-18 15:34:43 +0530

    setup done

--- src/components/QueryDemo.jsx
+++ src/components/QueryDemo.jsx
@@ -1,6 +1,4 @@
 
-import { useQuery } from '@tanstack/react-query'
-import {useState,useEffect} from 'react'
 
 const url="https://jsonplaceholder.typicode.com/posts?_limit=5"
 
@@ -8,34 +6,20 @@ const url="https://jsonplaceholder.typicode.com/posts?_limit=5"
 
 const fetchData=async()=>{
 
-        const res=await fetch(url)
-        
-        const response=await res.json()
-        return response
+ 
     }
 
 
 const QueryDemo = () => {
 
-  const [isLoadData,setIsLoadData]=useState(false)
 
   
-const {data:posts,error,isLoading}=useQuery({queryKey:["posts"],queryFn:fetchData,enabled:isLoadData})
  
   
+
   return (
-    <div>
-{isLoading && <p>Loading...</p>}
-{error && <p>Failed to fetch...</p>}
-      <ul>
-        {posts && posts.map((post)=>{
-          return <li key={post.id}>{post.title}</li>
-        })}
-
-      </ul>
-
-      <button onClick={()=>setIsLoadData(true)}>Get Data</button>
-      
+      <div>
+        Hello
     </div>
   )
 }
